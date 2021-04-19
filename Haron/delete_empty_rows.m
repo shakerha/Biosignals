f336114843caea2581bb8ac13d4f7529f5515df3
shakerha <52924299+shakerha@users.noreply.github.com>
2021-04-19 10:53:31 +0200

    Delete delete_empty_rows.m

--- Haron/delete_empty_rows.m
+++
@@ -1,9 +0,0 @@
-%delete empty rows
-
-C=readcell('founded_medicine_test.xls');
-C(:,[2]) = [];
-empty    = cellfun('isclass', C, 'missing');
-C(empty) = [];
-celldisp(C)
-list= cellfun(@(x) x(1:end-1,1), C, 'UniformOutput', false);
-writecell(C,'founded_medicine_folder_ohne.xls') 
